219b80d5f56f4d4bd46447b346c70705b7be6898
GulamMonsurKhan <gulammonsurkhan@gmail.com>
2025-06-05 15:13:06 -0400

    Enabled SDL2 rendering, able to display IBM logo

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,60 @@
 #include <iostream>
+#include <SDL2/SDL.h>
 #include <chip8.hpp>
 
-int main()  {
-     Chip8 chip8;
+#define ON_COLOR 0xDC143CFF //True Crimson
+#define OFF_COLOR 0x1E1E1EFF //Graphite Grey
 
+const int SCREEN_WIDTH = 64;
+const int SCREEN_HEIGHT = 32;
+const int SCALE = 12;
+
+int main()  {
+     
+     Chip8 chip8;
      chip8.Initialize();
      chip8.LoadROM("roms/IBM_Logo.ch8");
 
-     const uint8_t* memory = chip8.GetMemory();
+     SDL_Window *window = SDL_CreateWindow("CHIP-8 Emulator",
+          SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
+          SCREEN_WIDTH * SCALE, SCREEN_HEIGHT * SCALE, 
+          SDL_WINDOW_SHOWN);
+     
+     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1,  SDL_RENDERER_ACCELERATED);
 
-     std::cout << "\nROM loaded and memory initialized." << std::endl;
+     SDL_Texture *texture = SDL_CreateTexture(renderer,
+          SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING,
+          SCREEN_WIDTH, SCREEN_HEIGHT);
 
-     for(int i = 0; i < 10; ++i){
-          chip8.EmulateCycle();
+     bool running = true;
+     SDL_Event event;
+
+     while (running) {
+          while (SDL_PollEvent(&event)) {
+               if (event.type == SDL_QUIT) {
+                    running = false;
+               }
+               chip8.EmulateCycle();
+
+               uint32_t pixels[SCREEN_WIDTH * SCREEN_HEIGHT];
+               for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; ++i) {
+                    pixels[i] = chip8.gfx[i] ? ON_COLOR : OFF_COLOR;
+               }
+
+               SDL_UpdateTexture(texture, nullptr, pixels, SCREEN_WIDTH * sizeof(uint32_t));
+               SDL_RenderClear(renderer);
+               SDL_RenderCopy(renderer, texture, nullptr, nullptr);
+               SDL_RenderPresent(renderer);
+
+               SDL_Delay(16);
+          }
+
      }
+
+     SDL_DestroyTexture(texture);
+     SDL_DestroyRenderer(renderer);
+     SDL_DestroyWindow(window);
+     SDL_Quit();
 
      return 0;
 }
