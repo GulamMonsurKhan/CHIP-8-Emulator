719c756654ab07f89e05e8f583080035a562dc26
GulamMonsurKhan <gulammonsurkhan@gmail.com>
2025-06-10 17:45:31 -0400

    Added in keypad input, handled basic timer logic. Added in multiple instructions per frame

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 const int SCREEN_WIDTH = 64;
 const int SCREEN_HEIGHT = 32;
 const int SCALE = 12;
+const int INSTRUCTIONS_PER_FRAME = 15;
 
 int main(int argc, char* argv[])  {
 
@@ -51,13 +52,64 @@ int main(int argc, char* argv[])  {
      bool running = true;
      SDL_Event event;
 
+     uint32_t lastTimerUpdate = SDL_GetTicks();
+
      while (running) { //Main emulation loop
           while (SDL_PollEvent(&event)) { //Input handling, check SDL events
                if (event.type == SDL_QUIT) {
                     running = false;
                }
+               else if (event.type == SDL_KEYDOWN) {
+                    switch (event.key.keysym.sym) {
+                         case SDLK_x: chip8.keypad[0x0] = 1; break;
+                         case SDLK_1: chip8.keypad[0x1] = 1; break;
+                         case SDLK_2: chip8.keypad[0x2] = 1; break;
+                         case SDLK_3: chip8.keypad[0x3] = 1; break;
+                         case SDLK_q: chip8.keypad[0x4] = 1; break;
+                         case SDLK_w: chip8.keypad[0x5] = 1; break;
+                         case SDLK_e: chip8.keypad[0x6] = 1; break;
+                         case SDLK_a: chip8.keypad[0x7] = 1; break;
+                         case SDLK_s: chip8.keypad[0x8] = 1; break;
+                         case SDLK_d: chip8.keypad[0x9] = 1; break;
+                         case SDLK_z: chip8.keypad[0xA] = 1; break;
+                         case SDLK_c: chip8.keypad[0xB] = 1; break;
+                         case SDLK_4: chip8.keypad[0xC] = 1; break;
+                         case SDLK_r: chip8.keypad[0xD] = 1; break;
+                         case SDLK_f: chip8.keypad[0xE] = 1; break;
+                         case SDLK_v: chip8.keypad[0xF] = 1; break;
+                    }
+               }
+               else if (event.type == SDL_KEYUP) {
+                    switch (event.key.keysym.sym) {
+                         case SDLK_x: chip8.keypad[0x0] = 0; break;
+                         case SDLK_1: chip8.keypad[0x1] = 0; break;
+                         case SDLK_2: chip8.keypad[0x2] = 0; break;
+                         case SDLK_3: chip8.keypad[0x3] = 0; break;
+                         case SDLK_q: chip8.keypad[0x4] = 0; break;
+                         case SDLK_w: chip8.keypad[0x5] = 0; break;
+                         case SDLK_e: chip8.keypad[0x6] = 0; break;
+                         case SDLK_a: chip8.keypad[0x7] = 0; break;
+                         case SDLK_s: chip8.keypad[0x8] = 0; break;
+                         case SDLK_d: chip8.keypad[0x9] = 0; break;
+                         case SDLK_z: chip8.keypad[0xA] = 0; break;
+                         case SDLK_c: chip8.keypad[0xB] = 0; break;
+                         case SDLK_4: chip8.keypad[0xC] = 0; break;
+                         case SDLK_r: chip8.keypad[0xD] = 0; break;
+                         case SDLK_f: chip8.keypad[0xE] = 0; break;
+                         case SDLK_v: chip8.keypad[0xF] = 0; break;
+                    }
+               }
           }
-          chip8.EmulateCycle();
+
+          for (int i = 0; i < INSTRUCTIONS_PER_FRAME; ++i) {
+               chip8.EmulateCycle();
+          }
+
+          uint32_t currentTicks = SDL_GetTicks();
+          if (currentTicks - lastTimerUpdate >= 1000 /60 ) { //16ms
+               chip8.TickTimers();
+               lastTimerUpdate = currentTicks;
+          }
 
           uint32_t pixels[SCREEN_WIDTH * SCREEN_HEIGHT]; //CHIP-8 graphics buffer to SDL pixel buffer
           for (int i = 0; i < SCREEN_WIDTH * SCREEN_HEIGHT; ++i) {
