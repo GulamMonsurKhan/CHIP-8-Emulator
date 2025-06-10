0cc86935026aececa14285e035668a3dae2b44dd
GulamMonsurKhan <gulammonsurkhan@gmail.com>
2025-06-10 17:44:21 -0400

    Void function declaration for timers. Public declarations for keypress logic --> FX0A

--- include/chip8.hpp
+++ include/chip8.hpp
@@ -9,10 +9,15 @@ public:
     void Initialize();
     void LoadROM(const std::filesystem::path& filepath);
     void EmulateCycle();
+    void TickTimers();
     uint32_t gfx[64 * 32];
     uint8_t keypad[16]{};
     uint8_t* GetMemory() 
         {return memory;}
+    bool waitingForKey = false;
+    uint8_t waitingRegister = 0;
+    int8_t pressedKey = -1; 
+
 
 private:
     uint16_t pc{};
